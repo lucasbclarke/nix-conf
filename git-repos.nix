87b6739483016eca7d01147136a2336213b9c573
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-05-14 18:05:03 +1000

    Update git-repos.nix

--- git-repos.nix
+++ git-repos.nix
@@ -3,12 +3,13 @@
 pkgs.writeShellScriptBin "git-repos"
 ''
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
-sudo cp dotfiles/.zshrc ..
-sudo cp dotfiles/.tmux.conf ..
+sudo cp dotfiles/.zshrc ~/.zshrc
+sudo cp dotfiles/.tmux.conf ~/.tmux.conf
 mkdir ~/.config/i3
 sudo cp dotfiles/i3/config ~/.config/i3/config
 mkdir ~/.config/ghostty
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+tmux
 ''
