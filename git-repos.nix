6a61c3f9a222f983b6fb71a320a99e9bef389971
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-13 16:49:24 +1000

    maybe this will fix issues

--- git-repos.nix
+++ git-repos.nix
@@ -3,10 +3,16 @@
 pkgs.writeShellScriptBin "git-repos"
 ''
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
+sleep 10
 sudo cp dotfiles/.zshrc ..
+sleep 10
 sudo cp dotfiles/.tmux.conf ..
+sleep 10
 sudo cp dotfiles/i3/config ~/.config/i3/config
+sleep 10
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
+sleep 10
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
+sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ''
