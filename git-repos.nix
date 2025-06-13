07925485904100a0da02bd202b3cfdb8a65aad89
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-14 08:23:38 +1000

    reboot after installation

--- git-repos.nix
+++ git-repos.nix
@@ -10,4 +10,5 @@ sudo cp dotfiles/i3/config ~/.config/i3/
 mkdir ~/.config/ghostty/
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
+reboot
 ''
