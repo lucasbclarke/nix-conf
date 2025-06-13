34dd79e2211dea086fc87c0a86526cc10e39b45d
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-13 20:56:15 +1000

    Update git-repos.nix

--- git-repos.nix
+++ git-repos.nix
@@ -6,6 +6,7 @@ ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
 sudo cp dotfiles/.zshrc ~/
 sudo cp dotfiles/.tmux.conf ~/
 sudo cp dotfiles/i3/config ~/.config/i3/
+mkdir ~/.config/ghostty/
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
 ''
