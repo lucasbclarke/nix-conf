d735d983e9c7e3ba5a4e07fc1217b7e7515eac1f
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-13 17:37:08 +1000

    potential fix to commands not running after ohmyzsh installation

--- git-repos.nix
+++ git-repos.nix
@@ -3,9 +3,10 @@
 pkgs.writeShellScriptBin "git-repos"
 ''
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+export SHELL=$(which zsh)
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
-sudo cp dotfiles/.zshrc ..
-sudo cp dotfiles/.tmux.conf ..
+sudo cp dotfiles/.zshrc ~/
+sudo cp dotfiles/.tmux.conf ~/
 sudo cp dotfiles/i3/config ~/.config/i3/config
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
