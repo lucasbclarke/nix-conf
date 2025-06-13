a820994bee4778d8e41d533cc160fab4cd553636
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-13 21:51:55 +1000

    forgot the -r

--- git-repos.nix
+++ git-repos.nix
@@ -5,7 +5,7 @@ pkgs.writeShellScriptBin "git-repos"
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
 sudo cp dotfiles/.zshrc ~/
 sudo cp dotfiles/.tmux.conf ~/
-sudo cp dotfiles/.tmux ~/
+sudo cp -r dotfiles/.tmux ~/
 sudo cp dotfiles/i3/config ~/.config/i3/
 mkdir ~/.config/ghostty/
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
