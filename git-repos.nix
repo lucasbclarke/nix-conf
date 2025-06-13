ca2c59bf5f1d313ff2aa1435146f4973107cb263
Lucas Clarke <125113849+lucasbclarke@users.noreply.github.com>
2025-06-13 17:13:38 +1000

    reordering commands

--- git-repos.nix
+++ git-repos.nix
@@ -2,17 +2,11 @@
 
 pkgs.writeShellScriptBin "git-repos"
 ''
-${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
-sleep 10
-sudo cp dotfiles/.zshrc ..
-sleep 10
-sudo cp dotfiles/.tmux.conf ..
-sleep 10
-sudo cp dotfiles/i3/config ~/.config/i3/config
-sleep 10
-sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
-sleep 10
-${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
-sleep 10
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+${pkgs.git}/bin/git clone https://github.com/lucasbclarke/dotfiles
+sudo cp dotfiles/.zshrc ..
+sudo cp dotfiles/.tmux.conf ..
+sudo cp dotfiles/i3/config ~/.config/i3/config
+sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
+${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
 ''
