6aa585b7d9676bc7a0fe1a6f29aafd691a1d8da2
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-06-15 11:48:29 +1000

    making ghostty default terminal so that open in terminal works in thunar

--- configuration.nix
+++ configuration.nix
@@ -118,6 +118,10 @@
      (import ./ohmyzsh.nix {inherit pkgs;})
   ];
 
+  environment.variables = {
+    TERMINAL = "ghostty";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
