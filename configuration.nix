635fbc69a2a8afee0d7f6d3c339a5cfab02c42c7
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-06-17 18:49:58 +1000

    iwctl for networking

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,7 @@
 
   services.cloudflare-warp.enable = true;
 
+  networking.wireless.iwd.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
