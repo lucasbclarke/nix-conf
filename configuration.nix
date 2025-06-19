9597d3e532388b11c4197a39cc4b282af7bd81fb
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-06-19 20:16:54 +1000

    potential fix for usb not mounting

--- configuration.nix
+++ configuration.nix
@@ -117,7 +117,8 @@
   environment.systemPackages = with pkgs; [
      ghostty sqlite tldr fzf xdotool brave xfce.exo xfce.xfce4-settings
      unzip arduino-ide discord zls gcc cloudflare-warp neofetch
-     simple-scan pavucontrol screenkey vokoscreen-ng vlc
+     simple-scan pavucontrol screenkey vokoscreen-ng vlc usbutils
+     udiskie udisks
      (import ./git-repos.nix {inherit pkgs;})
      (import ./sud.nix {inherit pkgs;})
      (import ./ohmyzsh.nix {inherit pkgs;})
@@ -127,6 +128,7 @@
     enable = true;
     package = pkgs.gvfs;
   };
+  services.udisks2.enable = true;
 
   services.cloudflare-warp.enable = true;
 
