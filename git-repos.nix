0e4f59e346189fae1054e73ab22af896757e868c
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-06-21 17:39:03 +1000

    making the game with raylib work

--- git-repos.nix
+++ git-repos.nix
@@ -10,7 +10,11 @@ sudo cp dotfiles/i3/config ~/.config/i3/
 mkdir ~/.config/ghostty/
 sudo cp dotfiles/ghostty/config ~/.config/ghostty/config
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/code
+${pkgs.git}/bin/git clone https://github.com/lucasbclarke/game-project ~/code/projects/zig/game-project/
 ${pkgs.git}/bin/git clone https://github.com/lucasbclarke/nvim ~/.config/nvim
+${pkgs.git}/bin/git clone https://github.com/lucasbclarke/raylib-repo
+sudo mv raylib-repo/raylib .
+sudo mv raylib-repo/raylib-zig .
 curl https://ziglang.org/download/0.14.1/zig-x86_64-linux-0.14.1.tar.xz -o zig-x86_64-linux-0.14.1.tar.xz
 tar -xf zig-x86_64-linux-0.14.1.tar.xz
 sudo ln -s ~/zig-x86_64-linux-0.14.1/zig /usr/bin/zig
