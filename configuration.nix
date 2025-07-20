e76fd3cb52cfd0c0522ff0d8a325268cd9abfc65
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-07-20 14:55:10 +1000

    seperated zls installation from git repositories installation

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,12 @@
       ./hardware-configuration.nix
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda/";
+  boot.loader.grub.useOSProber = true;
 
   time.hardwareClockInLocalTime = true;
 
--- git-repos.nix
+++ git-repos.nix
@@ -21,26 +21,5 @@ mkdir zig-latest-linux-x86_64
 tar -xf zig-latest-linux-x86_64.tar.xz --strip-components=1 -C zig-latest-linux-x86_64
 sudo rm -rf /usr/bin/zig
 sudo ln -s ~/zig-latest-linux-x86_64/zig /usr/bin/zig
-
-
-if [ -d "$HOME/zls_source_temp" ]; then
-    rm -rf "$HOME/zls_source_temp"
-fi
-
-git clone "https://github.com/zigtools/zls.git" "$HOME/zls_source_temp"
-
-pushd "$HOME/zls_source_temp"
-zig build
-popd
-
-ZLS_EXEC_PATH="$HOME/zls_source_temp/zig-out/bin/zls"
-
-mkdir -p "$HOME/zig-latest-linux-x86_64/bin"
-mv "$HOME/zls_source_temp/zig-out/bin/zls" "$HOME/zig-latest-linux-x86_64/bin/zls"
-
-sudo rm -f /usr/bin/zls
-
-sudo ln -s "$HOME/zig-latest-linux-x86_64/bin/zls" /usr/bin/zls
-
-rm -rf "$HOME/zls_source_temp"
+reboot
 ''
