56eecbff4cb463742ee3ad10bc85a03df4a333e4
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-09-04 19:45:30 +1000

    fixed directory issue

--- winapps-setup.nix
+++ winapps-setup.nix
@@ -4,5 +4,5 @@ pkgs.writeShellScriptBin "winapps-setup"
 ''
 cd ~
 ${pkgs.git}/bin/git clone https://github.com/winapps-org/winapps.git
-sudo cp -r ~/dotfiles/winapps/. /usr/bin/.
+sudo cp -r ~/dotfiles/winapps/. /usr/bin/
 ''
