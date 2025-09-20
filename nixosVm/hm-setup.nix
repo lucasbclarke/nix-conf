b5c2c176e7b6c69498adbb1e5740dbf7c157c01a
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-09-21 06:25:21 +1000

    wrote a new script to configure home-manager with one command, still have to copy it to nixosGrub and nixosSystemD after testing

---
+++ nixosVm/hm-setup.nix
@@ -0,0 +1,8 @@
+{ pkgs }:
+
+pkgs.writeShellScriptBin "hm-setup"
+''
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+''
