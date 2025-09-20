50ce8e428b6790e8900c34f2a76d516f1e1a4c91
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-09-21 08:12:39 +1000

    added home-manager switch command at the end of hm-setup

--- nixosVm/hm-setup.nix
+++ nixosVm/hm-setup.nix
@@ -6,4 +6,5 @@ nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix-shell '<home-manager>' -A install
+home-manager switch --flake .
 ''
