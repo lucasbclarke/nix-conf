c132b459dfbf9ce8a7b5d9695ed5b132e9adb9db
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-09-26 14:53:51 +1000

    added cmp mappings and signs for gitsigns, still need to work on cpm actually displaying correct information

--- usr/nvim.nix
+++ usr/nvim.nix
@@ -11,7 +11,7 @@
 
     extraConfigLua = ''
       local servers = {
-	--zls = {},
+	zls = {},
 	nixd = {},
 	nil_ls = {},
 
@@ -36,54 +36,18 @@
 		     filetypes = server_config.filetypes,
       }
     end
+      
 
+      --------  CMP  --------
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       require('luasnip.loaders.from_vscode')
       luasnip.config.setup {}
 
     cmp.setup {
-      snippet = {
-	expand = function(args)
-	  luasnip.lsp_expand(args.body)
-	  end,
-      },
 	      completion = {
 		completeopt = 'menu,menuone,noinsert',
 	      },
-	      mapping = cmp.mapping.preset.insert {
-		['<C-d>'] = cmp.mapping.scroll_docs(-4),
-		['<C-u>'] = cmp.mapping.scroll_docs(4),
-		['<C-Space>'] = cmp.mapping.complete {},
-		['<CR>'] = cmp.mapping.confirm {
-		  behavior = cmp.ConfirmBehavior.Replace,
-		  select = true,
-		},
-		['<Tab>'] = cmp.mapping(function(fallback)
-		    if cmp.visible() then
-		    cmp.select_next_item()
-		    elseif luasnip.expand_or_locally_jumpable() then
-		    luasnip.expand_or_jump()
-		    else
-		    fallback()
-		    end
-		    end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-		    if cmp.visible() then
-		    cmp.select_prev_item()
-		    elseif luasnip.locally_jumpable(-1) then
-		    luasnip.jump(-1)
-		    else
-		    fallback()
-		    end
-		    end, { 'i', 's' })
-	      },
-	      sources = {
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' },
-		{ name = 'path' },
-	      },
-
 
 	      sorting = {
 		comparators = {
@@ -98,7 +62,12 @@
 	      },
     }
       config = function()
-	local lsp_zero = require("lsp-zero")
+	local l        add.text = "+";
+        change.text = "~";
+        changedelete.text = "~";
+        delete.text = "_";
+        topdelete.text = "‾";
+        untracked.text = "┆";sp_zero = require("lsp-zero")
 
 	lsp_zero.on_attach(function(client, bufnr)
 	    -- see :help lsp-zero-keybindings to learn the available actions
@@ -119,7 +88,60 @@
     plugins = {
 	treesitter.enable = true;
 	treesitter-textobjects.enable = true;
-	cmp.enable = true;
+
+	cmp = {
+	    enable = true;
+	    settings = {
+		mapping = {
+		  "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+		  "<C-u>" = "cmp.mapping.scroll_docs(4)";
+		  "<C-Space>" = "cmp.mapping.complete({})";
+		  "<CR>" = ''
+		    cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }, {"i", "c"})
+		  '';
+		  "<tab>" = ''
+		    cmp.mapping(function(fallback)
+		    if cmp.visible() then
+		      cmp.select_next_item()
+		    elseif luasnip.expand_or_locally_jumpable() then
+		      luasnip.expand_or_jump()
+		    else
+		      fallback()
+		    end
+		  end, { 'i', 's' }),
+		  '';
+
+		"<S-Tab>" = '' cmp.mapping(function(fallback)
+		    if cmp.visible() then
+		      cmp.select_prev_item()
+		    elseif luasnip.locally_jumpable(-1) then
+		      luasnip.jump(-1)
+		    else
+		      fallback()
+		    end
+		  end, { 'i', 's' })
+		    '';
+		};
+
+		snippet = {
+		  expand = ''
+		    function(args)
+		      require("luasnip").lsp_expand(args.body)
+		    end
+		    '';
+		};
+		sources = [
+		  { name = "nvim_lsp"; }
+		  {
+		    name = "luasnip";
+		    option = { show_autosnippets = true; };
+		  }
+		  { name = "path"; }
+		  { name = "buffer"; }
+		];
+	    };
+	};
+
 	luasnip.enable = true;
 	cmp_luasnip.enable = true;
 	cmp-nvim-lsp.enable = true;
@@ -131,74 +153,16 @@
 
 	gitsigns = {
 	  enable = true;
-	  luaConfig.post = ''
-	    opts = {
+	  settings = {
 	      signs = {
-		add = { text = '+' },
-		change = { text = '~' },
-		delete = { text = '_' },
-		topdelete = { text = '?' },
-		changedelete = { text = '~' },
-	      },
-	    on_attach = function(bufnr)
-	      local gs = package.loaded.gitsigns
-	      
-	      local function map(mode, l, r, opts)
-		opts = opts or {}
-		opts.buffer = bufnr
-		vim.keymap.set(mode, l, r, opts)
-	      end
-
-	      map({ 'n', 'v' }, ']c', function()
-		  if vim.wo.diff then
-		    return ']c'
-		  end
-		  vim.schedule(function()
-		    gs.next_hunk()
-		  end)
-		  return '<Ignore>'
-	      end, { expr = true, desc = 'Jump to next hunk' })
-
-	      map({ 'n', 'v' }, '[c', function()
-		  if vim.wo.diff then
-		    return '[c'
-		  end
-		  vim.schedule(function()
-		    gs.prev_hunk()
-		  end)
-		  return '<Ignore>'
-		end, { expr = true, desc = 'Jump to previous hunk' })
-
-	      map('v', '<leader>hs', function()
-		  gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-	      end, { desc = 'stage git hunk' })
-	      map('v', '<leader>hr', function()
-		  gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-	      end, { desc = 'reset git hunk' })
-
-	      map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-	      map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-	      map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-	      map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-	      map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-	      map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-	      map('n', '<leader>hb', function()
-		  gs.blame_line { full = false }
-	      end, { desc = 'git blame line' })
-	      map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-	      map('n', '<leader>hD', function()
-		  gs.diffthis '~'
-	      end, { desc = 'git diff against last commit' })
-
-	      map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-	      map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-	      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-	    end
-	  }
-
-
-	  '';
+		add.text = "+";
+		change.text = "~";
+		changedelete.text = "~";
+		delete.text = "_";
+		topdelete.text = "?";
+		untracked.text = "┆";
+	      };
+	  };
 	};
     };
     
@@ -207,6 +171,6 @@
       number = true;         
       relativenumber = true;
       shiftwidth = 2;        
-  };
+    };
   };
 }
