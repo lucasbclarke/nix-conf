2776b46c8e88b9f33392b5e1be65eceaab6c99a3
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-11-25 19:55:16 +1100

    cleaned up home.nix, removed comments and unneeded blocks

--- home.nix
+++ home.nix
@@ -16,37 +16,12 @@
   home.homeDirectory = "/home/lucas";
   home.stateVersion = "25.05"; 
 
-  home.packages = with pkgs; [
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-  ];
-
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   home.sessionVariables = {
     EDITOR = "nvim";
-    # Force dark mode for GTK applications
     GTK_THEME = "Adwaita-dark";
-    # Set color scheme preference to dark
     COLORTERM = "truecolor";
-    # Additional dark mode environment variables
     GTK_APPLICATION_PREFER_DARK_THEME = "1";
     QT_QPA_PLATFORMTHEME = "gtk2";
-    # Additional dark mode settings
     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
     QT_SCALE_FACTOR = "1";
   };
@@ -94,7 +69,6 @@
       name = "capitaine-cursors";
       package = pkgs.capitaine-cursors;
     };
-    # Force dark mode for GTK applications
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
     };
@@ -124,11 +98,8 @@
       settings = {
         main.font = "monospace:size=14";
       };
-
-      
   };
 
   programs.nixvim.enable = true;
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
